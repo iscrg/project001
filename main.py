99812e5dcc9808132ac6cc27d55f9c978691b83d
frosesss <ispjxfer@mail.ru>
2023-03-11 14:32:36 +0700

    ver. 1.0

---
+++ main.py
@@ -0,0 +1,22 @@
+import ru_local
+
+# Setting constants
+start_dist = 16637000000
+voyager_speed = 38241
+signal_speed = 299792458 * 2.236936  # Conversion from meters per second to miles per hour.
+
+print(ru_local.START_MSG)
+
+# Get number of days since the set date.
+days = int(input(ru_local.INPUT_MSG))
+
+ml_dist = start_dist + days * voyager_speed
+km_dist = ml_dist * 1.61
+astro_unit_dist = ml_dist * 1.07578 * 10**(-8)
+signal_time = ml_dist / signal_speed
+
+print('\n',
+      ru_local.OUT_ML_DIST, ml_dist, '\n',
+      ru_local.OUT_KM_DIST, km_dist, '\n',
+      ru_local.OUT_ASTRO_UNIT_DIST, astro_unit_dist, '\n',
+      ru_local.SIGNAL_TIME, signal_time)
