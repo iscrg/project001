eaff5fc0b3aae21c9caf95d284f9cd1bbe54c88e
Ivan <ispjxfer@mail.ru>
2023-03-13 09:30:45 +0700

    Update main.py
    fix

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import ru_local
 
 # Setting constants
-start_dist = 16637000000
-voyager_speed = 38241
+START_DIST = 16637000000
+VOYAGER_SPEED = 38241
 # Conversion from meters per second to miles per hour.
-signal_speed = 299792458 * 2.236936  
+SIGNAL_SPEED = 299792458 * 2.236936  
 
 print(ru_local.START_MSG)
 
 # Get number of days since the set date.
 days = int(input(ru_local.INPUT_MSG))
 
-ml_dist = start_dist + 24 * days * voyager_speed
+ml_dist = START_DIST + 24 * days * VOYAGER_SPEED
 km_dist = ml_dist * 1.61
 astro_unit_dist = ml_dist * 1.07578 * 10**(-8)
-signal_time = ml_dist / signal_speed
+signal_time = ml_dist / SIGNAL_SPEED
 
 print('\n',
       ru_local.OUT_ML_DIST, ml_dist, '\n',
